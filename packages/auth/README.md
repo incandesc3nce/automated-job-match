c277e90ae049ef11893bb50083e260f1268ec1e5
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-19 18:24:55 +0700

    feat: create auth package

---
+++ packages/auth/README.md
@@ -0,0 +1,3 @@
+# auth
+
+Package for handling authentication logic, including password hashing and JWT management. It uses the `argon2` library for secure password hashing and `jsonwebtoken` for generating and verifying JWTs. This package is used by the API to authenticate users and protect routes.
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,39 @@
+import { hash, verify } from 'argon2';
+import jwt from 'jsonwebtoken';
+import type { UUID } from 'node:crypto';
+
+export type JwtPayload = {
+  userId: UUID;
+};
+
+const HASH_SECRET = process.env['HASH_SECRET']!;
+const JWT_SECRET = process.env['JWT_SECRET']!;
+const expiresIn = '3d';
+
+// Hashing and verifying passwords using argon2
+export const hashPassword = async (password: string): Promise<string> => {
+  const hashedPassword = await hash(password, { secret: Buffer.from(HASH_SECRET) });
+  return hashedPassword;
+};
+
+export const verifyPassword = async (
+  password: string,
+  hashedPassword: string,
+): Promise<boolean> => {
+  const isValid = await verify(hashedPassword, password, {
+    secret: Buffer.from(HASH_SECRET),
+  });
+  return isValid;
+};
+
+// Generating and verifying JWTs using jsonwebtoken
+export const signJwt = (payload: JwtPayload) => {
+  return jwt.sign(payload, JWT_SECRET, {
+    expiresIn,
+  });
+};
+
+export const verifyJwt = (token: string): JwtPayload => {
+  const decoded =  jwt.verify(token, JWT_SECRET) as JwtPayload;
+  return decoded;
+};
