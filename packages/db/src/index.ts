89fea1d44624003664e3aedc62293138c172f5e5
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-19 19:14:03 +0700

    fix: correctly export package files

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 
-const connectionString = process.env['DB_URL']!
+const connectionString = process.env['DB_URL']!;
 
 export const db = drizzle(connectionString, { schema });
 export * from './schema';
+export { eq, lt, lte, gt, gte, ne } from 'drizzle-orm';
