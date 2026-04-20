392625248147b5ce3ded7019fb394ef16f821bdc
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-20 12:46:10 +0700

    feat: create auth middleware

---
+++ apps/api/src/middlewares/authMiddleware.ts
@@ -0,0 +1,27 @@
+import { UnauthorizedError } from '@/utils/APIError';
+import { verifyJwt } from '@career-ai/auth';
+import { createMiddleware } from 'hono/factory';
+import { getCookie } from 'hono/cookie';
+
+declare module 'hono' {
+  interface ContextVariableMap {
+    userId: string;
+  }
+}
+
+export const auth = createMiddleware(async (c, next) => {
+  const token = getCookie(c, 'token');
+
+  if (!token) {
+    throw new UnauthorizedError('Authentication token is missing');
+  }
+
+  try {
+    const decoded = verifyJwt(token);
+    c.set('userId', decoded.userId);
+  } catch (err) {
+    throw new UnauthorizedError('Invalid authentication token');
+  }
+
+  await next();
+});
