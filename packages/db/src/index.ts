5a1284b870c90336a2114cc7da9c4b22a926bd0a
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-24 23:18:29 +0700

    feat: add more operators to db exports

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,8 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 import * as schema from './schema';
 
 const connectionString = process.env['DB_URL']!;
+const client = postgres(connectionString);
 
-export const db = drizzle(connectionString, { schema });
+export const db = drizzle(client, { schema });
 export * from './schema';
-export { eq, lt, lte, gt, gte, ne } from 'drizzle-orm';
+export { eq, lt, lte, gt, gte, ne, and, or, sql, asc, desc } from 'drizzle-orm';
