69c7877f1e5032f441c428cbef41869ea279a694
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-25 21:38:51 +0700

    feat: add typography component

---
+++ apps/website/src/components/ui/typography.tsx
@@ -0,0 +1,31 @@
+interface TypographyProps {
+  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'span';
+  variant: 'default' | 'lead' | 'large' | 'small' | 'muted';
+  className?: string;
+}
+
+const typographyVariants = {
+  h1: 'scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance md:text',
+  h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+  h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
+  h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
+  p: 'leading-7 [&:not(:first-child)]:mt-6',
+  span: '',
+  default: '',
+  lead: 'text-lg font-normal md:text-xl',
+  large: 'text-lg font-semibold md:text-xl',
+  small: 'text-sm font-medium leading-none',
+  muted: 'text-sm text-muted-foreground',
+} as const;
+
+export const Typography = ({
+  tag = 'p',
+  variant = 'default',
+  className,
+}: TypographyProps) => {
+  const Tag = tag;
+  const tagClass = typographyVariants[tag];
+  const variantClass = typographyVariants[variant];
+
+  return <Tag className={`${tagClass} ${variantClass} ${className || ''}`} />;
+};
