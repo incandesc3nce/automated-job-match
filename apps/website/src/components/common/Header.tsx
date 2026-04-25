d4375975aade7a33df55f4f871c82b3706f35faa
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-25 22:25:42 +0700

    feat: add link to homepage in header

--- apps/website/src/components/common/Header.tsx
+++ apps/website/src/components/common/Header.tsx
@@ -8,12 +8,14 @@ export const Header = async () => {
   return (
     <div className="flex justify-center sticky top-0 z-20">
       <header className="flex items-center justify-between py-2 px-4 mt-4 w-full md:w-3/4 gap-4 bg-blue-100/80 rounded-lg border-2 border-mist-300">
-        <Typography tag="h3">
-          <Typography tag="span" accent>
-            Career
-          </Typography>{' '}
-          AI
-        </Typography>
+        <Link href='/'>
+          <Typography tag="h3">
+            <Typography tag="span" accent>
+              Career
+            </Typography>{' '}
+            AI
+          </Typography>
+        </Link>
         <div className="flex items-center justify-end gap-4">
           {!isLoggedIn && (
             <>
