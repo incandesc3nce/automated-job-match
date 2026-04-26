f6d0f083d322a31cb899543465a0a649df4e5941
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-26 22:59:13 +0700

    feat: add redirect to dashboard if user is already logged in on auth pages

--- apps/website/src/app/(auth)/layout.tsx
+++ apps/website/src/app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
 import { Header } from '@/components/common/Header';
+import { getCurrentSession } from '@/lib/getCurrentSession';
 import { ChildrenProps } from '@/types/ChildrenProps';
+import { redirect } from 'next/navigation';
 
-export default function AuthLayout({ children }: ChildrenProps) {
+export default async function AuthLayout({ children }: ChildrenProps) {
+  const user = await getCurrentSession();
+  if (user) {
+    redirect('/dashboard');
+  }
+
   return (
     <div className="flex flex-col gap-16">
       <Header />
