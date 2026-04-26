3ddf31969a76f078dc3a3bad959ca61d2e684fa5
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-26 23:40:20 +0700

    feat: add logout functionality

---
+++ apps/website/src/actions/logout.ts
@@ -0,0 +1,10 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export const logout = async () => {
+  (await cookies()).delete('token');
+
+  return redirect('/');
+};
---
+++ apps/website/src/app/logout/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { logout } from '@/actions/logout';
+import { useEffect } from 'react';
+
+export default function LogoutPage() {
+  useEffect(() => {
+    logout();
+  }, []);
+
+  return (
+    <div>
+      <p>Вы вышли из системы. Пожалуйста, подождите...</p>
+    </div>
+  );
+}
