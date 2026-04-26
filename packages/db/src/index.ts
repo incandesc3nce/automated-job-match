1a5350aa76e089fd90f2ce4683cc063fe13dd941
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-04-26 22:02:39 +0700

    build: containerize API app with Docker

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
 import * as schema from './schema';
 
 const connectionString = process.env['DB_URL']!;
-const client = postgres(connectionString);
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(connectionString, { schema });
 export * from './schema';
-export { eq, lt, lte, gt, gte, ne, and, or, sql, asc, desc } from 'drizzle-orm';
+export * from 'drizzle-orm';
