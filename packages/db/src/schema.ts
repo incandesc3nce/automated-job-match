9c184060cca433e4db66161b5d054466fd5f26b5
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-05-01 00:43:59 +0700

    feat: update jobs schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from 'drizzle-orm';
-import { pgTable, uuid, varchar, timestamp, integer } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, varchar, timestamp, integer, boolean } from 'drizzle-orm/pg-core';
 
 const id = uuid('id').defaultRandom().primaryKey();
 const createdAt = timestamp('created_at').defaultNow().notNull();
@@ -46,9 +46,33 @@ export const cvsRelations = relations(cvs, ({ one }) => ({
   }),
 }));
 
-// TODO: add fields
+// TODO: review fields, maybe they would require an update after other scrapers are written
 export const jobs = pgTable('jobs', {
   id: id,
+  source: varchar('source', { length: 255 }).notNull(),
+  title: varchar('title', { length: 255 }).notNull(),
+  companyName: varchar('company_name', { length: 255 }).notNull(),
+  location: varchar('location', { length: 255 }).notNull(),
+  experience: varchar('experience', { length: 255 }).notNull(),
+  workFormat: varchar('work_format', {
+    length: 20,
+    enum: ['remote', 'hybrid', 'onsite'],
+  }).notNull(),
+  salaryFrom: varchar('salary_from', { length: 50 }),
+  salaryTo: varchar('salary_to', { length: 50 }),
+  salaryExtra: varchar('salary_extra', { length: 255 }),
+  description: varchar('description', { length: 5000 }).notNull(),
+  skills: varchar('skills', { length: 255 }).array().notNull(),
+  postedAt: timestamp('posted_at'),
+  fetchedAt: timestamp('fetched_at').defaultNow().notNull(),
+  createdAt: createdAt,
+  embeddingStatus: varchar('embedding_status', {
+    length: 20,
+    enum: ['pending', 'completed', 'failed'],
+  })
+    .default('pending')
+    .notNull(),
+  hidden: boolean('hidden').default(false).notNull(),
 });
 
 // TODO: add fields
