58d2bc42ea68db56bc5a212555f7c3d33ca42d69
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-05-03 16:08:32 +0700

    feat(queue): add shorten description queue

--- packages/queue/src/index.ts
+++ packages/queue/src/index.ts
@@ -1,19 +1,22 @@
-import { Queue } from 'bullmq';
+import { Job, Queue } from 'bullmq';
 
-export type VectorizeJobPayload = {
+type JobId = {
   jobId: string;
 };
 
-export type VectorizeCvPayload = {
+type CvId = {
   cvId: string;
 };
 
-export type MatchJobCvPayload = {
-  jobId: string;
-  cvId: string;
-};
+export type ShortenDescriptionPayload = JobId;
+export type VectorizeJobPayload = JobId;
+export type VectorizeCvPayload = CvId;
+export type MatchJobCvPayload = JobId & CvId;
 
 export const scrapeJobsQueue = new Queue<string>('scrape-jobs');
+export const shortenDescriptionQueue = new Queue<ShortenDescriptionPayload>(
+  'shorten-description',
+);
 export const vectorizeJobQueue = new Queue<VectorizeJobPayload>('vectorize-job');
 export const vectorizeCvQueue = new Queue<VectorizeCvPayload>('vectorize-cv');
 export const matchJobCvQueue = new Queue<MatchJobCvPayload>('match-job-cv');
