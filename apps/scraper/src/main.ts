3656242a98b6a6e6441f82fa8c120fa6d2611149
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-05-03 14:56:12 +0700

    feat: add cron to schedule hh scraping flow and upsert jobs into database

--- apps/scraper/src/main.ts
+++ apps/scraper/src/main.ts
@@ -1,5 +1,45 @@
-import { HhAdapter } from "./adapters/HhAdapter";
+import { db, jobs } from '@career-ai/db';
+import { HhAdapter } from './adapters/HhAdapter';
+import { cron } from 'bun';
 
 const hhAdapter = new HhAdapter();
 
-const jobs = await hhAdapter.fetchJobs();
+cron('0 0 * * *', async () => {
+  console.log('Starting hh scraping flow...');
+  const startTime = Date.now();
+  try {
+    const normalizedJobs = await hhAdapter.flow();
+    console.log(
+      `hh scraping flow completed. Total unique jobs fetched: ${normalizedJobs.length}`,
+    );
+    console.log(`Inserting ${normalizedJobs.length} jobs into the database...`);
+    await db
+      .insert(jobs)
+      .values([...normalizedJobs.values()])
+      .onConflictDoUpdate({
+        target: [jobs.source, jobs.externalId],
+        set: {
+          title: jobs.title,
+          companyName: jobs.companyName,
+          location: jobs.location,
+          experience: jobs.experience,
+          workFormat: jobs.workFormat,
+          salaryFrom: jobs.salaryFrom,
+          salaryTo: jobs.salaryTo,
+          salaryExtra: jobs.salaryExtra,
+          description: jobs.description,
+          skills: jobs.skills,
+          fetchedAt: new Date(),
+        },
+      });
+    console.log('hh Jobs inserted/updated successfully');
+  } catch (error) {
+    console.error('Error during HH scraping flow:', error);
+  } finally {
+    const endTime = Date.now();
+    const duration = ((endTime - startTime) / 1000).toFixed(2);
+    console.log(`hh scraping flow finished in ${duration} seconds.`);
+  }
+});
+
+console.log('Scraper service started. Waiting for scheduled tasks...');
