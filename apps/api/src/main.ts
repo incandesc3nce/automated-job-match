b9adf49c5900837580e303699dd63bdb905e3e4e
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-05-11 23:31:04 +0700

    refactor(api): remove redundant server start log

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,8 +2,6 @@ import app from './app';
 
 const port = process.env['PORT'] ? Number(process.env['PORT']) : 5000;
 
-console.log(`🔥 Server is running at http://localhost:${port}`);
-
 export default {
   port,
   hostname: '0.0.0.0',
